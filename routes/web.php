cff78ab327b9afe9f9a0322065b4cf7d3eff6497
Arya Jati <aryajati29@gmail.com>
2023-07-30 01:35:39 +0700

    minor update

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,6 @@ Route::get('/index', function () {
     return view('/index');
 })->name('beranda');
 
-Route::get('/index/publikasi', function () {
-    return view('/index/publikasi');
-})->name('berita');
-
-Route::get('/index/bacaan', function () {
-    return view('/index/bacaanmateri');
-})->name('bacaan');
-
-Route::get('/index/katalog', function () {
-    return view('/index/katalog');
-})->name('katalog');
-
 Route::get('/tentangkami', function () {
     return view('/tentangkami');
 })->name('tentangkami');
@@ -48,12 +36,6 @@ Route::get('/tentangkami', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
 });
-
-
-Route::get('/edu-article', function () {
-    return view('index.edu_article');
-});
-
 
 
 
@@ -72,19 +54,16 @@ Route::post('/store', 'TrixController@store');
 
 
 // News
-
-Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
-Route::post('/news', [NewsController::class, 'store'])->name('news.store');
-Route::get('/news/{id}/edit', [NewsController::class, 'edit'])->name('news.edit');
+Route::middleware('auth')->group(function () {
+    Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
+    Route::post('/news', [NewsController::class, 'store'])->name('news.store');
+    Route::get('/news/{id}/edit', [NewsController::class, 'edit'])->name('news.edit');
+    Route::put('/news/{id}', [NewsController::class, 'update'])->name('news.update');
+    Route::delete('/news/{id}', [NewsController::class, 'destroy'])->name('news.destroy');
+});
 Route::get('/news/{id}/{slug}', [NewsController::class, 'show'])->name('news.show');
-Route::put('/news/{id}', [NewsController::class, 'update'])->name('news.update');
-Route::delete('/news/{id}', [NewsController::class, 'destroy'])->name('news.destroy');
 Route::get('/news/search', [NewsController::class, 'search'])->name('news.search');
 Route::get('/news', [NewsController::class, 'index'])->name('news.index');
-
-Route::get('/news/article', function () {
-    return view('news.article');
-});
 
 // Index
 Route::get('/index', [IndexController::class, 'index'])->name('beranda');
@@ -96,28 +75,33 @@ Route::get('/index/faq', function () {
 
 // Educational
 
-Route::get('/educational/create', [EducationalController::class, 'create'])->name('educational.create');
-Route::post('/educational', [EducationalController::class, 'store'])->name('educational.store');
-Route::get('/educational/{id}/edit', [EducationalController::class, 'edit'])->name('educational.edit');
+Route::middleware('auth')->group(function () {
+    Route::get('/educational/create', [EducationalController::class, 'create'])->name('educational.create');
+    Route::post('/educational', [EducationalController::class, 'store'])->name('educational.store');
+    Route::get('/educational/{id}/edit', [EducationalController::class, 'edit'])->name('educational.edit');
+    Route::put('/educational/{id}', [EducationalController::class, 'update'])->name('educational.update');
+    Route::delete('/educational/{id}', [EducationalController::class, 'destroy'])->name('educational.destroy');
+});
 Route::get('/educational/{id}/{slug}', [EducationalController::class, 'show'])->name('educational.show');
-Route::put('/educational/{id}', [EducationalController::class, 'update'])->name('educational.update');
-Route::delete('/educational/{id}', [EducationalController::class, 'destroy'])->name('educational.destroy');
 Route::get('/educational/search', [EducationalController::class, 'search'])->name('educational.search');
 Route::get('/educational', [EducationalController::class, 'index'])->name('educational.index');
 
 // Profile
 
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+});
 
 // Book
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
+    Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
+});
 Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
-Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
-Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
-Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
